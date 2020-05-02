eda901639a88dbecd16097dcfea2cf47e2a4452d
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-05-02 15:04:14 -0400

    added CSS functionality to Home page. Included cards for images, and JS for functionality of navbar and button selection

---
+++ portfolio.js
@@ -0,0 +1,47 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+$("#aboutBtn").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#aboutMe").offset().top - 85
+    },
+        'slow');
+    console.log("scroll")
+});
+$("#skillsBtn").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#skills").offset().top - 90
+    },
+        'slow');
+    console.log("scroll")
+});
+$("#portfolioBtn").click(function () {
+    $('html, body').animate({
+        scrollTop: $(".portfolio").offset().top - 95
+    },
+        'slow');
+    console.log("scroll")
+});
+$("#contactBtn").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#contactMe").offset().top - 400
+    },
+        'slow');
+    console.log("scroll")
+});
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
